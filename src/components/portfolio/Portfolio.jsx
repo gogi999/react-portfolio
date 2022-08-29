05cc3badc3a680eea13be59a918281ff8458b8f6
gogi999 <g.petrovic9@gmail.com>
2022-08-29 14:02:08 +0200

    Finished project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,22 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PortfolioList from '../portfolioList/PortfolioList';
 import './portfolio.scss';
+import {
+    featuredPortfolio,
+    webPortfolio,
+    mobilePortfolio,
+    designPortfolio,
+    contentPortfolio,
+} from '../../data';
 
 const Portfolio = () => {
+    const [selected, setSelected] = useState('featured');
+    const [data, setData] = useState([]);
+
+    const list = [
+        {
+            id: "featured",
+            title: "Featured",
+        },
+        {
+            id: "web",
+            title: "Web App",
+        },
+        {
+            id: "mobile",
+            title: "Mobile App",
+        },
+        {
+            id: "design",
+            title: "Design",
+        },
+        {
+            id: "content",
+            title: "Content",
+        },
+    ];
+
+    useEffect(() => {
+        switch (selected) {
+            case 'featured': 
+                setData(featuredPortfolio);
+                break;
+            case 'web': 
+                setData(webPortfolio);
+                break;
+            case 'mobile': 
+                setData(mobilePortfolio);
+                break;
+            case 'design': 
+                setData(designPortfolio);
+                break;
+            case 'content': 
+                setData(contentPortfolio);
+                break;
+            default:
+                setData(featuredPortfolio);
+        }
+    }, [selected]);
+
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <ul>
-                <li className="active">Featured</li>
-                <li>Web App</li>
-                <li>Mobile App</li>
-                <li>Design</li>
-                <li>Branding</li>
+                {list.map((item) => (
+                    <PortfolioList 
+                        title={item.title} 
+                        key={item.id} 
+                        active={selected === item.id}
+                        setSelected={setSelected}
+                        id={item.id}
+                    />
+                ))}
             </ul>
             <div className="container">
-                <div className="item">
-                    <img src="assets/globe.png" alt="" />
-                    <h3>Places App</h3>
-                </div>
+                {data.map((d) => (
+                    <div className="item" key={d.id}>
+                        <img src={d.img} alt="" />
+                        <h3>{d.title}</h3>
+                    </div>
+                ))}
             </div>
         </div>
     );
